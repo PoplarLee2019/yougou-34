0851f918e28df8ca99580643643e35511fd74123
xxxx <xxx@xx.com>
2019-08-16 22:29:44 +0800

    首页与分类页完成

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,3 +1,4 @@
+import request from "../../utils/request.js";
 // pages/category/index.js
 Page({
 
@@ -5,62 +6,35 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 总的数据
+    navs: [],
+
+    // 索引
+    current: 0
 
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function () {
+    request({
+      url: "/categories"
+    }).then(res => {
+      const { message } = res.data;
 
+      this.setData({
+        navs: message
+      })
+    })
   },
+  //  菜单栏点击时候触发
+  handleChange(event) {
+    const { index } = event.currentTarget.dataset;
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    this.setData({
+      current: index
+    })
   }
+
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,71 @@
+import request from "../../utils/request.js";
 Page({
-  
+  data: {
+    autoplay: true,
+    // 轮播图的数据
+    imgUrls: [
+      // 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      // 'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      // 'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ],
+    // 菜单数据
+    menus: [],
+    // 楼层数据
+    floors: []
+  },
+
+  onLoad() {
+    request({
+      url: "/home/swiperdata"
+    }).then(res => {
+      // 返回的数组
+      const { message } = res.data;
+      // 修改imgUrls
+      this.setData({
+        imgUrls: message
+      })
+    });
+        // 请求菜单的数据
+    request({
+      url: "/home/catitems"
+    }).then(res => {
+      const {message} = res.data;
+
+      this.setData({
+        menus: message
+      })
+    })
+
+    // 请求楼层数据
+    request({
+      url: "/home/floordata"
+    }).then(res => {
+      const {message} = res.data;
+
+      this.setData({
+        floors: message
+      })
+    });
+    // 请求菜单的数据
+    request({
+      url: "/home/catitems"
+    }).then(res => {
+      const { message } = res.data;
+
+      this.setData({
+        menus: message
+      })
+    })
+
+    // 请求楼层数据
+    request({
+      url: "/home/floordata"
+    }).then(res => {
+      const { message } = res.data;
+
+      this.setData({
+        floors: message
+      })
+    })
+  }
 })
